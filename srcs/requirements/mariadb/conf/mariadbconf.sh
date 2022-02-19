7f2d6c89d5f3e3aab1fc3f47c634b60d25e37d2f
ubuntu <ubuntu@ubunu2004.linuxvmimagrs.local>
2022-02-20 02:14:19 +0530

    [MOKELLAH] setting up mariadb

--- srcs/requirements/mariadb/conf/mariadbconf.sh
+++ srcs/requirements/mariadb/conf/mariadbconf.sh
@@ -1,2 +1,5 @@
-#bin/sh
+#!bin/sh
 
+service mysql start
+mysql -uroot -proot -e "GRANT ALL ON *.* TO 'mokellah'@'localhost' IDENTIFIED BY '123456' WITH GRANT OPTION"
+mysql -uroot -proot -e "FLUSH PRIVILEGES"

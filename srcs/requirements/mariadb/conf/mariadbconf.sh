30eaa69500df719d8cc01589b0485450034bc7c5
ubuntu <ubuntu@ubunu2004.linuxvmimagrs.local>
2022-02-22 01:17:53 +0530

    mariadb: [DONE]

--- srcs/requirements/mariadb/conf/mariadbconf.sh
+++ srcs/requirements/mariadb/conf/mariadbconf.sh
@@ -3,3 +3,4 @@
 service mysql start
 mysql -uroot -proot -e "GRANT ALL ON *.* TO 'mokellah'@'localhost' IDENTIFIED BY '123456' WITH GRANT OPTION"
 mysql -uroot -proot -e "FLUSH PRIVILEGES"
+mysqld_safe

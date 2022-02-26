5c2f0644c7c8c6f765e65e4320d2493f82320c62
mokellat <kellatis55@gmail.com>
2022-02-27 05:19:15 +0700

    docker compose

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 run:
+	docker volume rm srcs_db srcs_wordpress
+	sudo mkdir -p /home/mokellah/data
 	docker-compose -f srcs/docker-compose.yml up
--- srcs/requirements/mariadb/conf/mariadb_conf.sh
+++ srcs/requirements/mariadb/conf/mariadb_conf.sh
@@ -3,4 +3,5 @@
 service mysql start
 mysql -uroot -proot -e "GRANT ALL ON *.* TO 'mokellah'@'localhost' IDENTIFIED BY '123456' WITH GRANT OPTION"
 mysql -uroot -proot -e "FLUSH PRIVILEGES"
+service mysql stop
 mysqld_safe

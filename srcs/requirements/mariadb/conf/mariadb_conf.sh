90304aeb04dd2e623b8297e9564a4f85dd328817
mallah-elmehdi <elmehdimallah@gmail.com>
2022-03-02 20:42:29 +0530

    just clean

--- srcs/requirements/mariadb/conf/mariadb_conf.sh
+++ srcs/requirements/mariadb/conf/mariadb_conf.sh
@@ -1,7 +1,6 @@
 #!bin/sh
-
 service mysql start
-mysql -uroot -proot -e "GRANT ALL ON *.* TO 'mokellah'@'localhost' IDENTIFIED BY '123456' WITH GRANT OPTION"
+mysql -uroot -proot -e "GRANT ALL ON *.* TO '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_USER_PASSWORD}' WITH GRANT OPTION"
 mysql -uroot -proot -e "FLUSH PRIVILEGES"
 service mysql stop
 mysqld_safe
--- srcs/requirements/wordpress/conf/wp_config.sh
+++ srcs/requirements/wordpress/conf/wp_config.sh
@@ -1,7 +1,6 @@
 # !/bin/bash
-
 service php7.3-fpm start
 service php7.3-fpm stop
-# /etc/init.d/php7.0-fpm restart && nginx -g "daemon off;"
 php-fpm7.3 -F
+# /etc/init.d/php7.0-fpm restart && nginx -g "daemon off;"
 # tail -f /dev/null

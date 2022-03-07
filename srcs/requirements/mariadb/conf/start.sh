389efbd4797f259a27aa20e38055a3ac56d8263f
mallah-elmehdi <elmehdimallah@gmail.com>
2022-03-08 00:48:02 +0530

    Makefile updated

--- srcs/requirements/mariadb/conf/start.sh
+++ srcs/requirements/mariadb/conf/start.sh
@@ -4,12 +4,9 @@ then
 	service mysql start
 	mysql -e "CREATE DATABASE $DB"
 	mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION"
-	#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
 	mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');"
-	#mysql -e "FLUSH PRIVILEGES"
 	service mysql stop
 	mysqld_safe
 else
-	#mysqld_safe
 	mysqld_safe --skip-grant-table
 fi
